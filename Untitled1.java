9640dd523a225d2c6f9a3875d9428886174d0cd8
VijayB12 <132127680+VijayB12@users.noreply.github.com>
2024-07-20 12:51:34 +0530

    Add files via upload
    Java Programme

---
+++ Untitled1.java
@@ -0,0 +1,19 @@
+//simple addition program
+
+public class Addition {
+
+public static void main(String args[]) { 
+
+int a,b,c; a=5;//input of first value
+
+System.out.println("enter first value "+"a="+a);
+
+b=45;// input of second value
+
+System.out.println("enter second value "+"b="+b);
+
+c=a+b;// addition of two numbers operation
+
+System.out.println("addition of a+b ="+c);
+
+}        }
